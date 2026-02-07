3d739353a393f4bc8eef31cf92d2d9e39ad63705
Khushi sharma <ks0541929@gmail.com>
2026-02-07 12:13:08 +0530

    day2

--- Day_02.js
+++ Day_02.js
@@ -16,9 +16,7 @@ Math.round    = Nearest integer round    👉 Math.random(); // 0.0 to 0.999...
 
 /* 
 Q1) Accept the principal amount (P), annual interest rate (r), the number of years (t), and number of times interest is compounded per year (n). Write a program to calculate the compound interest. The formula to calculate compound interest is:
-
 A = P × (1 + r/n)^(n × t)
-
 Where A is the amount of money accumulated after n years, including interest. P is the principal amount (the initial sum of money). r is the annual interest rate (in decimal). t is the time the money is invested for in years. n is the number of times that interest is compounded per year. The compound interest is then calculated as CI = A – P.
 */
 
@@ -32,7 +30,7 @@ function CI(P, r, t, n) {
 CI(1000,0.5,10,4);
 
 /*
-Q2) ccept the lengths of the three sides of a triangle, write a program to calculate the area of the triangle using Heron’s formula. The formula states that if the sides of the triangle are a, b, and c, Where s is the semi-perimeter of the triangle.
+Q2) Accept the lengths of the three sides of a triangle, write a program to calculate the area of the triangle using Heron’s formula. The formula states that if the sides of the triangle are a, b, and c, Where s is the semi-perimeter of the triangle.
  */
 
 function AreaOfTraingle(a,b,c){
